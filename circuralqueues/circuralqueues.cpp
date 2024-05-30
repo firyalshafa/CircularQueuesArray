220040b95bfc30e74560734881cfb07514a301d9
firyalshafa <shafafiryal782@gmail.com>
2024-05-30 15:10:42 +0700

    mengecek jika antrian hanya memiliki satu element

--- circuralqueues/circuralqueues.cpp
+++ circuralqueues/circuralqueues.cpp
@@ -49,6 +49,12 @@ public:
 			return;
 		}
 		cout << "\nthe element deleted from the queue is:" << queue_array[FRONT] << "\n";
+
+		// cek jika antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
 	}
 };
 
